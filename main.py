bf47ae67a0710b9f86258c6536b8a2b40f0ddfeb
Adash <darras.adrien@gmail.com>
2026-01-10 19:42:21 +0100

    Add souces folder, constants.py and engine.py
    J ai mis a jour le main . J ai crée le dossier "sources"  et j ai fait les fichiers pour les constantes et le moteur.
    
    Etat actuel : Ecran noir au lancement

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame as pg
 
-pg.init()
-ecran = pg.display.set_mode((300, 200))
-pg.display.set_caption("jeu")
-pg.time.delay(1000)
-pg.quit()
+from sources.engine import Game
+
+if __name__ == "__main__":
+	
+    Glade = Game()
+
+    Glade.run()
---
+++ sources/engine.py
@@ -0,0 +1,25 @@
+import pygame as pg
+from sources.constants import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((LARGEUR,HAUTEUR))
+        pg.display.set_caption("Glade")
+        self.horloge = pg.time.Clock()
+        self.play = True
+
+    def run(self):
+
+        while self.play:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.play = False
+
+            self.screen.fill(NOIR)
+            
+            pg.display.flip()
+
+            self.horloge.tick(FPS)
+
+        pg.quit()

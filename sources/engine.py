d637ef5c3ca8fdeefbe5d6c8e2893c8d4b686165
v1entram <havane1111@gmail.com>
2026-01-13 19:20:49 +0100

    Fixed implementation + speed variable
    fixé l'implémentation des mouvements et ajout du paramètre "vitesse" pour ce mouvement

--- sources/engine.py
+++ sources/engine.py
@@ -5,11 +5,11 @@ from sources.player import *
 class Game:
     def __init__(self):
         pg.init()
-        self.screen = pg.display.set_mode((LARGEUR,HAUTEUR))
+        self.screen = pg.display.set_mode((LARGEUR, HAUTEUR))
         pg.display.set_caption("Glade")
         self.horloge = pg.time.Clock()
         self.play = True
-        self.player = Player(400,300)
+        self.player = Player(400, 300)
 
     def run(self):
 
@@ -17,16 +17,10 @@ class Game:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.play = False
-<<<<<<< Updated upstream
-
-            self.screen.fill(BLANC)
-=======
             self.player.move()
             self.screen.fill(BLANC)
             self.screen.blit(self.player.image, (self.player.posix, self.player.posiy))
-            
->>>>>>> Stashed changes
-            
+
             pg.display.flip()
 
             self.horloge.tick(FPS)
--- sources/player.py
+++ sources/player.py
@@ -1,24 +1,26 @@
-import pygame 
+import pygame
 from sources.engine import *
 import sources.constants as cst
 
 pygame.init()
+
+
 class Player:
-    def __init__(self,x,y):
+    def __init__(self, x, y, speed=2):
         self.image = cst.Player_img
         self.rect = self.image.get_rect()
         self.posix = x
-        self.posiy = y 
-        self.speed = 2
+        self.posiy = y
+        self.speed = speed
+
     def move(self):
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                self.posix -= self.speed
-            if keys[pygame.K_RIGHT]:
-                self.posix += self.speed
-            if keys[pygame.K_UP]:
-                self.posiy -= self.speed
-            if keys[pygame.K_DOWN]:
-                self.posiy += self.speed
-            self.rect.topleft = (self.posix, self.posiy)
-    
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.posix -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.posix += self.speed
+        if keys[pygame.K_UP]:
+            self.posiy -= self.speed
+        if keys[pygame.K_DOWN]:
+            self.posiy += self.speed
+        self.rect.topleft = (self.posix, self.posiy)
